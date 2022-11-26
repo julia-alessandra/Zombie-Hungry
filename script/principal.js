b02d847096924c92d5c31f2f44c7afac9b2013c5
duda-abecassis <107891170+duda-abecassis@users.noreply.github.com>
2022-11-25 23:09:09 -0300

    não consegui mudar o texto das configuraçoes
    O fundo só funcionou colocando no js

--- script/principal.js
+++ script/principal.js
@@ -1,3 +1,12 @@
+//só está funcionando assim
+let body = document.querySelector("body")
+body.style.backgroundImage = 'url(imagens/fundo-tp2.png)'
+body.style.backgroundRepeat = 'no-repeat'
+body.style.backgroundPosition = 'left bottom'
+body.style.backgroundSize = 'cover'
+body.style.height = '100vh'
+
+//botoes de abrir e fechar
 let botaoEl = document.querySelectorAll(".botao");
 let fechar = document.querySelectorAll("#fechar");
 
@@ -34,3 +43,30 @@ for (let i = 0; i < botaoEl.length; i++) {
     siteFundo.classList.remove("invisivel")
   })
 }
+
+//configuraaçoes
+let nomeEl = document.querySelector("#nome")
+let nomeInput = document.querySelector("#nomeInput")
+
+nomeInput.addEventListener("keyup", (e) => {
+  nomeEl.innerHTML = nomeInput.value
+})
+
+
+let personagens = document.querySelectorAll("#personagemZombie")
+let imagemPrincipal = document.querySelector("#zombie")
+let imagemPequena = document.querySelector("#perfilZombie")
+
+for (let i = 0; i < personagens.length; i++) {
+  personagens[i].addEventListener("click", function () {
+    imagemPrincipal.src = personagens[i].value
+    imagemPequena.src = personagens[i].value
+  })}
+
+let classico = document.querySelector("#classico")
+let among = document.querySelector("#among")
+let cefet = document.querySelector("#cefet")
+let hasan = document.querySelector("#hasan")
+let peralta = document.querySelector("#peralta")
+let taylor = document.querySelector("#taylor")
+
