5ab998eeb78fa254a481f1cfe88c7a894a75510d
julia-alessandra <julialessandra3@gmail.com>
2022-11-28 00:23:26 -0300

    amanha eu arrumo

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # [Zombie Hungry](https://julia-alessandra.github.io/Zombie-Hungry/)
 
-> "nha" - Pou
+> "gjrjgtygj" - Zombie que perdeu um pedaço do cérebro
 
 Design criado pelo [canva](https://www.canva.com/design/DAFQdKBg6hg/UuxQvWdwhVdAIkhCUjWnDw/edit?utm_content=DAFQdKBg6hg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
 
-Mapa e desenhos do jogo: https://cypor.itch.io/12x12-rpg-tileset
+[Mapa e desenhos do jogo](https://cypor.itch.io/12x12-rpg-tileset)
+
+[Curso de canvas e jogos feito para esse jogo](https://chriscourses.com/courses)
 
 Projeto criado para o Trabalho Prático 2 da disciplina de "laboratório de programação web" do CEFET-MG.
 
--- script/jogo.js
+++ script/jogo.js
@@ -1,21 +1,55 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-//imagens
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const batalhaMap = [];
+for (let i = 0; i < batalhas.length; i += 70) {
+  batalhaMap.push(batalhas.slice(i, 70 + i));
+}
 
-//
+//console.log(batalhaMap);
+
+class barreira {
+  static width = 38;
+  static height = 38;
+  constructor({ posicao }) {
+    this.posicao = posicao;
+    this.width = 38;
+    this.height = 38;
+  }
+  draw() {
+    c.fillStyle = "pink";
+    c.fillRect(this.posicao.x, this.posicao.y, this.width, this.height);
+  }
+}
+const zonaBatalha = [];
+
+const off = {
+  x: -1,
+  y: -70,
+};
+
+batalhaMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 27989)
+      zonaBatalha.push(
+        new barreira({
+          posicao: {
+            x: j * barreira.width + off.x,
+            y: i * barreira.height + off.y,
+          },
+        })
+      );
+  });
+});
+//imagens
 const imagem = new Image();
 imagem.src = "./jogo/map.png";
 
 const jogador = new Image();
 jogador.src = "./jogo/playerDown.png";
 
-
-//posicao do fundo
 class sprite {
-  constructor({ posicao, imagem }) {
+  constructor({ posicao, velocidade, imagem }) {
     this.posicao = posicao;
     this.imagem = imagem;
   }
@@ -24,19 +58,14 @@ class sprite {
   }
 }
 
-
 const fundo = new sprite({
   posicao: {
     //essa posição é onde o personagem vai começar
-    x: -200,
-    y: -350,
+    x: off.x,
+    y: off.y,
   },
   imagem: imagem,
 });
-
-
-//personagem
-//personagem andar se botao for pressionado
 
 const botoes = {
   arrowUp: {
@@ -53,7 +82,71 @@ const botoes = {
   },
 };
 
-let ultimo; //isso é pra apertar duas ao mesmo tempo
+const movimento = [fundo, ...zonaBatalha];
+
+function colisaoRetangulo({ retangulo1, retangulo2 }) {
+  return (
+    retangulo1.posicao.x + retangulo1.width >= retangulo2.posicao.x &&
+    retangulo1.posicao.x <= retangulo2.posicao.x + retangulo2.width &&
+    retangulo1.posicao.y <= retangulo2.posicao.y + retangulo1.height &&
+    retangulo1.posicao.y + retangulo1.height >= retangulo2.posicao.y
+  );
+}
+//as barreiras estap seguindo o personagem, n sei arrumar
+function animacao() {
+  window.requestAnimationFrame(animacao);
+  fundo.draw();
+  //teste.draw()
+  c.drawImage(
+    jogador,
+    0,
+    0,
+    jogador.width / 4,
+    jogador.height,
+    canvas.width / 2,
+    canvas.height / 2,
+    //não sei se isso é pixel, se for eu me mato
+    30,
+    40
+  );
+
+
+  for(let i=0; i<zonaBatalha.length; i++){
+    const batalhaZona = zonaBatalha(i)
+    if(
+      colisaoRetangulo({
+        retangulo1:jogador,
+        retangulo2: batalhaZona
+      })
+    ){
+      movimento = false
+      break
+    }
+  }
+
+  zonaBatalha.forEach((zonaBatalha) => {
+    zonaBatalha.draw();
+  });
+  //para o personagem andar, eu estou alterando a posicao do fundo!!!!
+  if (botoes.arrowDown.pressed && ultimo === "ArrowDown") {
+    movimento.forEach((movimento) => {
+      movimento.posicao.y -= 3;
+    });
+  } else if (botoes.arrowUp.pressed && ultimo === "ArrowUp") {
+    movimento.forEach((movimento) => {
+      movimento.posicao.y += 3;
+    });
+  } else if (botoes.arrowRight.pressed && ultimo === "ArrowRight") {
+    movimento.forEach((movimento) => {
+      movimento.posicao.x -= 3;
+    });
+  } else if (botoes.arrowLeft.pressed && ultimo === "ArrowLeft") {
+    movimento.forEach((movimento) => {
+      movimento.posicao.x += 3;
+    });
+  }
+}
+animacao();
 
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
@@ -74,9 +167,10 @@ window.addEventListener("keydown", (e) => {
       ultimo = "ArrowLeft";
       break;
   }
-  console.log(botoes);
+  //console.log(botoes);
 });
 
+let ultimo; //isso é pra apertar duas ao mesmo tempo
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "ArrowUp":
@@ -93,26 +187,3 @@ window.addEventListener("keyup", (e) => {
       break;
   }
 });
-
-function animacao() {
-  window.requestAnimationFrame(animacao);
-  fundo.draw();
-  c.drawImage(
-    jogador,
-    0,
-    0,
-    jogador.width / 4,
-    jogador.height,
-    canvas.width / 2,
-    canvas.height / 2,
-    30,
-    40
-  );
-
-  //para o personagem andar, eu estou alterando a posicao do fundo!!!!
-  if (botoes.arrowDown.pressed && ultimo === "ArrowDown") fundo.posicao.y -= 3;
-  else if (botoes.arrowUp.pressed && ultimo === "ArrowUp") fundo.posicao.y += 3;
-  else if (botoes.arrowRight.pressed && ultimo === "ArrowRight") fundo.posicao.x -= 3;
-  else if (botoes.arrowLeft.pressed && ultimo === "ArrowLeft") fundo.posicao.x += 3;
-}
-animacao();
